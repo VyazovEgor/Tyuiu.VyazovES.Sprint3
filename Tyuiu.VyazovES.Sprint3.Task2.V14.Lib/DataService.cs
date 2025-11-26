5cfd9c802f229a9d59e69328dd8e60c5c63b1a32
Егор Вязов <Tyuiu.VyazovES@outlook.com>
2025-11-27 00:51:14 +0500

    1

--- Tyuiu.VyazovES.Sprint3.Task2.V14.Lib/DataService.cs
+++ Tyuiu.VyazovES.Sprint3.Task2.V14.Lib/DataService.cs
@@ -5,12 +5,11 @@ namespace Tyuiu.VyazovES.Sprint3.Task2.V14.Lib
     public class DataService : ISprint3Task2V14
     {
         public double GetMultiplySeries(int x, int k1, int k2)
-        {  
-            double S = 0;
-
+        {
+            double S = 1;
             do
             {
-                S *= (x / k1) * (x / k1) * (x / k1);
+                S = S * ((double)x / k1) * ((double)x / k1) * ((double)x / k1);
                 k1++;
             }
             while (k1 <= k2);
