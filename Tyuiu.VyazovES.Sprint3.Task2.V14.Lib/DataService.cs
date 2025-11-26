aee23640a5c03ca257850109c3ca3897b94154ee
Егор Вязов <Tyuiu.VyazovES@outlook.com>
2025-11-27 00:46:07 +0500

    1

---
+++ Tyuiu.VyazovES.Sprint3.Task2.V14.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.VyazovES.Sprint3.Task2.V14.Lib
+{
+    public class DataService : ISprint3Task2V14
+    {
+        public double GetMultiplySeries(int x, int k1, int k2)
+        {  
+            double S = 0;
+
+            do
+            {
+                S *= (x / k1) * (x / k1) * (x / k1);
+                k1++;
+            }
+            while (k1 <= k2);
+
+            return Math.Round(S, 3);
+        
+    }
+    }
+}
---
+++ Tyuiu.VyazovES.Sprint3.Task2.V14/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.VyazovES.Sprint3.Task2.V14.Lib;
+
+DataService ds = new DataService();
+double s;
+
+int x = Convert.ToInt32(Console.ReadLine());
+int k1 = Convert.ToInt32(Console.ReadLine());
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+s = ds.GetMultiplySeries(x, k1, k2);
+
+
+Console.WriteLine(s);
+
+
+Console.ReadKey();
