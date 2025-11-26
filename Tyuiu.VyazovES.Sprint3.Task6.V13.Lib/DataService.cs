cf3ed70854dc0317360d06267bafb38f60a69d4f
Егор Вязов <Tyuiu.VyazovES@outlook.com>
2025-11-27 02:11:24 +0500

    1

---
+++ Tyuiu.VyazovES.Sprint3.Task6.V13.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.VyazovES.Sprint3.Task6.V13.Lib
+{
+    public class DataService : ISprint3Task6V13
+    {
+        public int GetSumTheDivisors(int a, int b)
+        {
+            int sum = 0;
+
+            for (int x = a; x <= b; x++)
+            {
+                for (int d = 1; d <= x; d++)
+                {
+                    if (x % d == 0 && d > 8)
+                    {
+                        sum += d;
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.VyazovES.Sprint3.Task6.V13/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.VyazovES.Sprint3.Task6.V13.Lib;
+
+DataService ds = new DataService();
+double s;
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+s = ds.GetSumTheDivisors(a, b);
+
+Console.WriteLine(s);
+
+
+Console.ReadKey();
